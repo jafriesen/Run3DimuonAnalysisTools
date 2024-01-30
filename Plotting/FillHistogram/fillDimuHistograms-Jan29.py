69c7c3a62c3a2836ed4aa8c6bd50db113bdb55c1
Jaren Friesen <jarenfriesen@gmail.com>
2024-01-29 23:43:23 -0500

    Update fillDimuHistograms-Jan29.py

--- Plotting/FillHistogram/fillDimuHistograms-Jan29.py
+++ Plotting/FillHistogram/fillDimuHistograms-Jan29.py
@@ -107,7 +107,7 @@ def fillHistogram():
 			for cut in eta_cut :
 				if abs(ev.eta) < eta_cut[cut][0] or abs(ev.eta) > eta_cut[cut][1] : continue
 				for i_selection in range(len(muon_selections)) :
-					if !(ev.muonID1[i_selection] and ev.muonID2[i_selection]) : continue
+					if not (ev.muonID1[i_selection] and ev.muonID2[i_selection]) : continue
 					if(verbose) : print(ev.pt, ev.eta, ev.muonID1[i_selection], ev.muonID2[i_selection], "massDimu_pt" + str(pt_bins[i]) + "to" + str(pt_bins[i+1]) + "_" + cut + "_" + muon_selections[i_selection])
 					config[i_bin][cut][muon_selections[i_selection]].Fill(ev.mass)
 
